15a6d42bc7fd1a933ea8291c9b811adfdf342b19
ianbautista <ianfbautista@gmail.com>
2020-07-15 14:56:03 -0700

    imported axios. added banner.js and constants.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
 import "./App.css";
+import Axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
-    </div>
-  );
+	return (
+		<div className="App">
+			<p>
+				Read through the instructions in the README.md file to build your NASA app! Have fun{" "}
+				<span role="img" aria-label="go!">
+					🚀
+				</span>
+				!
+			</p>
+		</div>
+	);
 }
 
 export default App;
