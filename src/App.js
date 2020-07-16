0b95e3013ee797a7df97229b498832077f7f59af
ianbautista <ianfbautista@gmail.com>
2020-07-16 08:47:23 -0700

    restructured. Fixed bugs.

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import "./App.css";
 import Axios from "axios";
 import { NASA_API_KEY, NASA_BASE_URL } from "./components/constants";
 import Banner from "./components/Banner";
-
+console.log(Banner);
 function App() {
 	const [photoOfTheDay, setPhotoOfTheDay] = useState({});
+	console.log(photoOfTheDay);
 	useEffect(() => {
 		Axios.get(`${NASA_BASE_URL}?api_key=${NASA_API_KEY}`)
 			.then((response) => {
+				console.log(response);
 				const photoObject = {
 					hdurl: response.data.hdurl,
 					explanation: response.data.explanation,
 					date: response.data.date,
 					title: response.data.title,
 				};
+				console.log(photoOfTheDay);
 				setPhotoOfTheDay(photoObject);
 			})
 			.catch((error) => {
@@ -22,13 +25,14 @@ function App() {
 			});
 	}, []);
 
+	console.log(photoOfTheDay);
 	return (
 		<div className="App">
 			{/* <img src="" /> */}
 			<h1> PHOTO OF THE DAY </h1>
-			<Banner photoOfTheDay={photoOfTheDay} />
+			<Banner photoOfTheDay={photoOfTheDay} setPhotoOfTheDay={setPhotoOfTheDay} />
+			{console.log(photoOfTheDay)}
 		</div>
 	);
 }
-
 export default App;
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,17 +1,19 @@
 import React from "react";
+import "../App.css";
 import Description from "./Description";
 
 function Banner(props) {
-	const { hdurl, title, explanation, date } = props;
+	console.log(props);
+	const { hdurl, title, explanation, date } = props.photoOfTheDay;
 
 	return (
-		<div>
-			<section className="banner">
+		<div className="banner">
+			<section className="imgContainer">
 				<img alt={title} src={hdurl} />
-				<div className="caption">
-					<Description explanation={explanation} title={title} date={date} />
-				</div>
 			</section>
+			<div className="description">
+				<Description explanation={explanation} title={title} date={date} />
+			</div>
 		</div>
 	);
 }
