8f105adde864acc2221f6dab0cf8bffb4a356312
ianbautista <ianfbautista@gmail.com>
2020-07-16 10:35:38 -0700

    added mars stretch

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import "./App.css";
 import Axios from "axios";
 import { NASA_API_KEY, NASA_BASE_URL } from "./components/constants";
 import Banner from "./components/Banner";
+import Mars from "./components/Mars";
 console.log(Banner);
 function App() {
 	const [photoOfTheDay, setPhotoOfTheDay] = useState({});
 	console.log(photoOfTheDay);
 	useEffect(() => {
-		Axios.get(`${NASA_BASE_URL}?api_key=${NASA_API_KEY}`)
+		Axios.get(`${NASA_BASE_URL}/planetary/apod?api_key=${NASA_API_KEY}`)
 			.then((response) => {
 				console.log(response);
 				const photoObject = {
@@ -24,14 +25,36 @@ function App() {
 				console.log(`Ooops! There's an Error. ${error}`);
 			});
 	}, []);
+	const [marsPhoto, setMarsPhoto] = useState("");
+	useEffect(() => {
+		Axios.get(
+			`${NASA_BASE_URL}/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${NASA_API_KEY}`,
+		)
+			.then((response) => {
+				console.log(response);
+				let marsArrayLength = response.data.photos.length;
+				console.log(marsArrayLength);
+				let randomized = Math.floor(Math.random() * (marsArrayLength + 1));
+				console.log(randomized);
+				const marsPhotoObject = {
+					imgSrc: response.data.photos[randomized].img_src,
+					earthDate: response.data.photos[randomized].earth_date,
+				};
+				setMarsPhoto(marsPhotoObject);
+			})
+			.catch((error) => {
+				console.log(`Ooops! There's an Error. ${error}`);
+			});
+	}, []);
 
 	console.log(photoOfTheDay);
 	return (
 		<div className="App">
-			{/* <img src="" /> */}
+			<img src="../public/nasalogo.png" alt="NASA Logo" />
 			<h1> PHOTO OF THE DAY </h1>
 			<Banner photoOfTheDay={photoOfTheDay} setPhotoOfTheDay={setPhotoOfTheDay} />
-			{console.log(photoOfTheDay)}
+			{/* {console.log(photoOfTheDay)} */}
+			<Mars marsPhoto={marsPhoto} setMarsPhoto={setMarsPhoto} />
 		</div>
 	);
 }
--- src/components/constants.js
+++ src/components/constants.js
@@ -1,2 +1,2 @@
 export const NASA_API_KEY = "0V2Vw4TEo8nNMJktA4H2Q55PR62VMYSp1Wd88FgC";
-export const NASA_BASE_URL = "https://api.nasa.gov/planetary/apod";
+export const NASA_BASE_URL = "https://api.nasa.gov";
